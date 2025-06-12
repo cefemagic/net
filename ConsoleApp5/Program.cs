ef7849f11a04513bd8f139ef26c59945006bd6a2
Developer One <cefemagic@gmail.com>
2025-06-12 12:07:48 -0500

    version 1

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("¿Cuántas preguntas deseas hacer?");
+if (int.TryParse(Console.ReadLine(), out int nPreguntas) && nPreguntas > 0)
+{
+    for (int i = 1; i <= nPreguntas; i++)
+    {
+        Console.WriteLine($"[{i}] Introduce tu fecha de nacimiento en formato ddMMyyyy:");
+        string? fechaInput = Console.ReadLine();
+
+        if (DateTime.TryParseExact(fechaInput, "ddMMyyyy", null, System.Globalization.DateTimeStyles.None, out DateTime fechaNacimiento))
+        {
+            DateTime hoy = DateTime.Today;
+            int edad = hoy.Year - fechaNacimiento.Year;
+
+            if (fechaNacimiento > hoy.AddYears(-edad))
+            {
+                edad--;
+            }
+
+            Console.WriteLine($"Tu edad es: {edad} años");
+        }
+        else
+        {
+            Console.WriteLine("Formato de fecha incorrecto. Por favor, usa ddMMyyyy.");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Número de preguntas no válido.");
+}
+
+Console.WriteLine("Presiona cualquier tecla para salir...");
+Console.ReadKey();
