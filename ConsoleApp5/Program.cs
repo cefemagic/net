b9e32b43338a84f62d1b07bcc3b6856b9f4e44a9
Developer One <cefemagic@gmail.com>
2025-06-13 17:48:53 -0500

    modificado 2

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,35 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine($"Introduce tu fecha de nacimiento en formato ddMMyyyy:");
+string? fechaInput = Console.ReadLine();
 
-if (int.TryParse(Console.ReadLine(), out int nPreguntas) && nPreguntas > 0)
+if (DateTime.TryParseExact(fechaInput, "ddMMyyyy", null, System.Globalization.DateTimeStyles.None, out DateTime fechaNacimiento))
 {
-    for (int i = 1; i <= nPreguntas; i++)
+    DateTime hoy = DateTime.Today;
+    int edad = hoy.Year - fechaNacimiento.Year;
+
+    if (fechaNacimiento > hoy.AddYears(-edad))
     {
-        Console.WriteLine($"[{i}] Introduce tu fecha de nacimiento en formato ddMMyyyy:");
-        string? fechaInput = Console.ReadLine();
-
-        if (DateTime.TryParseExact(fechaInput, "ddMMyyyy", null, System.Globalization.DateTimeStyles.None, out DateTime fechaNacimiento))
-        {
-            DateTime hoy = DateTime.Today;
-            int edad = hoy.Year - fechaNacimiento.Year;
-
-            if (fechaNacimiento > hoy.AddYears(-edad))
-            {
-                edad--;
-            }
-
-            Console.WriteLine($"Tu edad es: {edad} años");
-        }
-        else
-        {
-            Console.WriteLine("Formato de fecha incorrecto. Por favor, usa ddMMyyyy.");
-        }
+        edad--;
     }
+
+    Console.WriteLine($"Tu edad es: {edad} años");
 }
 else
 {
-    Console.WriteLine("Número de preguntas no válido.");
+    Console.WriteLine("Formato de fecha incorrecto. Por favor, usa ddMMyyyy...");
 }
+
 
 Console.WriteLine("Presiona cualquier tecla para salir...");
 Console.ReadKey();
--- ConsoleApp5/obj/Debug/net7.0/ConsoleApp5.AssemblyInfo.cs
+++ ConsoleApp5/obj/Debug/net7.0/ConsoleApp5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869f5b2271935e781e9083965261b3323f2fcb79")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
