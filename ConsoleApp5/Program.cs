c21020c8745fe5a98792104ce92196fa523c1639
Developer One <cefemagic@gmail.com>
2025-06-13 18:44:02 -0500

    "nuevos cambios"

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine($"Introduce tu fecha de nacimiento en formato ddMMyyyy:");
+Console.WriteLine($"Ingresa tu fecha de nacimiento en formato ddMMyyyy:");
 string? fechaInput = Console.ReadLine();
 
 if (DateTime.TryParseExact(fechaInput, "ddMMyyyy", null, System.Globalization.DateTimeStyles.None, out DateTime fechaNacimiento))
@@ -13,35 +13,9 @@ if (DateTime.TryParseExact(fechaInput, "ddMMyyyy", null, System.Globalization.Da
         edad--;
     }
 
-    Console.WriteLine($"Tu edad debe ser: {edad} años");
+    Console.WriteLine($"Tu tienes: {edad} años de edad");
 }
 else
 {
     Console.WriteLine("Formato de fecha incorrecto. Por favor, usa ddMMyyyy...");
 }
-=======
-    Console.WriteLine($"Introduce tu fecha de nacimiento en formato ddMMyyyy:");
-    string? fechaInput = Console.ReadLine();
-
-    if (DateTime.TryParseExact(fechaInput, "ddMMyyyy", null, System.Globalization.DateTimeStyles.None, out DateTime fechaNacimiento))
-    {
-        DateTime hoy = DateTime.Today;
-        int edad = hoy.Year - fechaNacimiento.Year;
-
-        if (fechaNacimiento > hoy.AddYears(-edad))
-        {
-            edad--;
-        }
-
-        Console.WriteLine($"Tu edad es: {edad} años");
-    }
-    else
-    {
-        Console.WriteLine("Formato de fecha incorrecto. Por favor, usa ddMMyyyy.");
-    }
-
->>>>>>> 391027465a1640741fad35a0c902d61e0c8277ae
-
-
-Console.WriteLine("Presiona cualquier tecla para salir...");
-Console.ReadKey();
