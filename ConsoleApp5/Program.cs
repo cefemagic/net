1baf2fb0c1aeb741e3ec20c4e9cde6354796485f
Developer One <cefemagic@gmail.com>
2025-06-13 17:06:05 -0500

    resolve

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,36 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Console.WriteLine("¿Cuántas preguntas deseas hacer?");
-if (int.TryParse(Console.ReadLine(), out int nPreguntas) && nPreguntas > 0)
-{
-    for (int i = 1; i <= nPreguntas; i++)
+
+    Console.WriteLine($"Introduce tu fecha de nacimiento en formato ddMMyyyy:");
+    string? fechaInput = Console.ReadLine();
+
+    if (DateTime.TryParseExact(fechaInput, "ddMMyyyy", null, System.Globalization.DateTimeStyles.None, out DateTime fechaNacimiento))
     {
-        Console.WriteLine($"[{i}] Introduce tu fecha de nacimiento en formato ddMMyyyy:");
-        string? fechaInput = Console.ReadLine();
+        DateTime hoy = DateTime.Today;
+        int edad = hoy.Year - fechaNacimiento.Year;
 
-        if (DateTime.TryParseExact(fechaInput, "ddMMyyyy", null, System.Globalization.DateTimeStyles.None, out DateTime fechaNacimiento))
+        if (fechaNacimiento > hoy.AddYears(-edad))
         {
-            DateTime hoy = DateTime.Today;
-            int edad = hoy.Year - fechaNacimiento.Year;
-
-            if (fechaNacimiento > hoy.AddYears(-edad))
-            {
-                edad--;
-            }
-
-            Console.WriteLine($"Tu edad es: {edad} años");
+            edad--;
         }
-        else
-        {
-            Console.WriteLine("Formato de fecha incorrecto. Por favor, usa ddMMyyyy.");
-        }
+
+        Console.WriteLine($"Tu edad es: {edad} años");
     }
-}
-else
-{
-    Console.WriteLine("Número de preguntas no válido.");
-}
+    else
+    {
+        Console.WriteLine("Formato de fecha incorrecto. Por favor, usa ddMMyyyy.");
+    }
+
 
 Console.WriteLine("Presiona cualquier tecla para salir...");
 Console.ReadKey();
